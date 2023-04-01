53e5575120abd27604ad2f1e6632bfc31ede3252
oussama-sbaa <sbaa.oussama07@gmail.com>
2023-04-01 17:04:58 +0200

    feat: UT of IPokedexTest interface Final Version - TP3 done successfully

--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -35,8 +35,9 @@ public class IPokedexTest {
     @Test
     @Tag("Pokedex")
     @DisplayName("Returns the number of pokemon this pokedex contains")
-    public void SizeTest(){
+    public void sizeTest(){
 
+        //Mock
         Mockito.doReturn(this.pokemons.size()).when(this.pokedex).size();
 
         //use of AssertJ in Junit5
@@ -47,10 +48,11 @@ public class IPokedexTest {
     @Test
     @Tag("Pokedex")
     @DisplayName("Locates the pokemon identified by the given <tt>id</tt>")
-    public void AddPokemonTest() {
+    public void addPokemonTest() {
 
         Pokemon newPokemon = new Pokemon(133, "Aquali", 186, 186, 260, 2729, 202, 5000, 4, 100.0);
 
+        //Mock
         //Mockito.doReturn(newPokemon.getIndex()).when(this.pokedex).addPokemon(newPokemon);
         Mockito.doReturn(this.pokemons.size()+1).when(this.pokedex).addPokemon(newPokemon);
         //assertThat(this.pokedex.addPokemon(newPokemon)).isEqualTo(newPokemon.getIndex());
@@ -61,11 +63,12 @@ public class IPokedexTest {
     @Test
     @Tag("Pokedex")
     @DisplayName("Returns an unmodifiable list of all pokemons this pokedex contains")
-    public void GetPokemonTest() throws PokedexException {
+    public void getPokemonTest() throws PokedexException {
 
 
         int aqualiIndex = 133, bulbizarreIndex = 0, firstInvalidIndex = 170, secondInvalidIndex = -20;
 
+        //Mock
         Mockito.doReturn(aquali).when(this.pokedex).getPokemon(aqualiIndex);
         Mockito.doReturn(bulbizarre).when(this.pokedex).getPokemon(bulbizarreIndex);
         Mockito.doThrow(new PokedexException("invalid given index")).when(this.pokedex).getPokemon(Mockito.intThat(i -> i < 0 || i > 150));
@@ -82,10 +85,13 @@ public class IPokedexTest {
 
 
     @Test
-    public void GetPokemonsTest(){
+    @Tag("Pokedex")
+    @DisplayName("Returns an unmodifiable list of all pokemons this pokedex contains")
+    public void getPokemonsTest(){
 
         List<Pokemon> TestPokemonsList = Collections.unmodifiableList(this.pokemons);
 
+        //Mock
         Mockito.doReturn(TestPokemonsList).when(this.pokedex).getPokemons();
 
         //Use of AssertJ in JUnit5
@@ -98,7 +104,9 @@ public class IPokedexTest {
 
 
     @Test
-    public void GetPokemonsOrdered(){
+    @Tag("Pokedex")
+    @DisplayName("Returns an unmodifiable list of all pokemons this pokedex contains")
+    public void getPokemonsOrdered(){
 
         PokemonComparators nComparator = PokemonComparators.NAME;
         PokemonComparators iComparator = PokemonComparators.INDEX;
@@ -115,7 +123,7 @@ public class IPokedexTest {
 
         List<Pokemon> edTestPokemonsList = Collections.unmodifiableList(new ArrayList<>());
 
-
+        //Mock
         Mockito.doReturn(Collections.unmodifiableList(nPokemonsOrdered)).when(this.pokedex).getPokemons(nComparator);
         Mockito.doReturn(Collections.unmodifiableList(iPokemonsOrdered)).when(this.pokedex).getPokemons(iComparator);
         Mockito.doReturn(Collections.unmodifiableList(cpPokemonsOrdered)).when(this.pokedex).getPokemons(cpComparator);

e9c722e5faabd04a5bdd556535e0b17830552976
oussama-sbaa <sbaa.oussama07@gmail.com>
2023-05-01 16:29:33 +0200

    fix: Re build project - Correct some issues

--- src/main/java/fr/univavignon/pokedex/api/PokemonTrainer.java
+++ src/main/java/fr/univavignon/pokedex/api/PokemonTrainer.java
@@ -2,7 +2,7 @@ package fr.univavignon.pokedex.api;
 
 /**
  * Trainer POJO.
- * 
+ *
  * @author fv
  */
 public class PokemonTrainer {
@@ -12,13 +12,13 @@ public class PokemonTrainer {
 
 	/** Trainer team. **/
 	private final Team team;
-	
+
 	/** Trainer pokedex. **/
 	private final IPokedex pokedex;
-	
+
 	/**
 	 * Default constructor.
-	 * 
+	 *
 	 * @param name Trainer name.
 	 * @param team Trainer team.
 	 * @param pokedex Trainer pokedex.
@@ -28,7 +28,7 @@ public class PokemonTrainer {
 		this.team = team;
 		this.pokedex = pokedex;
 	}
-	
+
 	/** Name getter. **/
 	public String getName() {
 		return name;
@@ -38,10 +38,10 @@ public class PokemonTrainer {
 	public Team getTeam() {
 		return team;
 	}
-	
+
 	/** Pokedex getter. **/
 	public IPokedex getPokedex() {
 		return pokedex;
 	}
-	
+
 }
